5c0bd38f664150cc842154d3dd402064d0a879bd
johnkkvnsn <0323-3554@lspu.edu.ph>
2026-05-12 23:14:20 +0800

    key

--- app.py
+++ app.py
@@ -426,7 +426,7 @@ def scan_meal(current_user):
         )
         result = client.run_workflow(
             workspace_name="john-lloyd-apao",
-            workflow_id="Detect-and-Classify",
+            workflow_id="detect-count-and-visualize",
             images={"image": temp_path},
             use_cache=True
         )
